6713b6b4e056b987573155c53271cd1236c95b37
Louie <louie.dartmoor.corpe@cern.ch>
2014-12-12 11:08:48 +0100

    cleared up

--- Puppi/plugins/PuppiProducer.cc
+++ Puppi/plugins/PuppiProducer.cc
@@ -52,54 +52,54 @@ PuppiProducer::~PuppiProducer(){
 // ------------------------------------------------------------------------------------------
 void PuppiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 	// Get PFCandidate Collection
-/*	edm::Handle<CandidateView> hPFProduct;
-	iEvent.getByLabel(fPFName,hPFProduct);
-	assert(hPFProduct.isValid());
-	const reco::CandidateView *PFCol = hPFProduct.product();*/
+	/*	edm::Handle<CandidateView> hPFProduct;
+			iEvent.getByLabel(fPFName,hPFProduct);
+			assert(hPFProduct.isValid());
+			const reco::CandidateView *PFCol = hPFProduct.product();*/
 
 	// Get vertex collection w/PV as the first entry?
-/*	edm::Handle<reco::VertexCollection> hVertexProduct;
-	iEvent.getByLabel(fPVName,hVertexProduct);
-	assert(hVertexProduct.isValid());
-	const reco::VertexCollection *pvCol = hVertexProduct.product(); //no need for PV coll anymore.
-	if (pvCol->size());*/
-Handle<View<pat::PackedCandidate> > pfCandidates;
-iEvent.getByToken(pfcandidateToken_,pfCandidates);
-const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
+	/*	edm::Handle<reco::VertexCollection> hVertexProduct;
+			iEvent.getByLabel(fPVName,hVertexProduct);
+			assert(hVertexProduct.isValid());
+			const reco::VertexCollection *pvCol = hVertexProduct.product(); //no need for PV coll anymore.
+			if (pvCol->size());*/
+	Handle<View<pat::PackedCandidate> > pfCandidates;
+	iEvent.getByToken(pfcandidateToken_,pfCandidates);
+	const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
 	//Fill the reco objects
 	fRecoObjCollection.clear();
 	Handle<View<reco::Vertex> > primaryVertices;
 	iEvent.getByToken(vertexToken_,primaryVertices);
 	const PtrVector<reco::Vertex>& pvPtrs = primaryVertices->ptrVector();
 
-		Handle<View<flashgg::DiPhotonCandidate> > diPhotons;
-		iEvent.getByToken(diPhotonToken_,diPhotons);
-		const PtrVector<flashgg::DiPhotonCandidate>& diPhotonPointers = diPhotons->ptrVector();
-		
-     edm::Ptr<reco::Vertex> lpv;
-		 int counter0=0;
-		 int counter1=0;
-		 int counter2=0;
-		 int counter3=0;
-		 int counter4=0;
-		 int counterFindVtx=0;
+	Handle<View<flashgg::DiPhotonCandidate> > diPhotons;
+	iEvent.getByToken(diPhotonToken_,diPhotons);
+	const PtrVector<flashgg::DiPhotonCandidate>& diPhotonPointers = diPhotons->ptrVector();
 
-		if(useFlashggVertex){
-			if(diPhotonPointers.size() >0){
-			 lpv = diPhotonPointers[0]->getVertex(); // The idea is to eventually loop over ALL diphoton canidates in future commit. FIXME
-			} else {
-			lpv = pvPtrs[0]; 
-			} 
+	edm::Ptr<reco::Vertex> lpv;
+	int counter0=0;
+	int counter1=0;
+	int counter2=0;
+	int counter3=0;
+	int counter4=0;
+	int counterFindVtx=0;
+
+	if(useFlashggVertex){
+		if(diPhotonPointers.size() >0){
+			lpv = diPhotonPointers[0]->getVertex(); // The idea is to eventually loop over ALL diphoton canidates in future commit. FIXME
 		} else {
+			lpv = pvPtrs[0]; 
+		} 
+	} else {
 		lpv = pvPtrs[0]; 
-		}
-		
-	std::cout << "DEBUG diphoton[0] position " << lpv->z() << ", 0th vtx pos " << pvPtrs[0]->z() << std::endl;	
+	}
+
+	//std::cout << "DEBUG diphoton[0] position " << lpv->z() << ", 0th vtx pos " << pvPtrs[0]->z() << std::endl;	
 	//for(CandidateView::const_iterator itPF = PFCol->begin(); itPF!=PFCol->end(); itPF++) {
 	for(unsigned int pfLoop = 0; pfLoop < PFCol.size() ; pfLoop ++) {
-	Ptr<pat::PackedCandidate> itPF = (PFCol[pfLoop]); 
-//	pat::PackedCandidate *itPF;
-//	itPF = *(PFCol[pfLoop]);
+		Ptr<pat::PackedCandidate> itPF = (PFCol[pfLoop]); 
+		//	pat::PackedCandidate *itPF;
+		//	itPF = *(PFCol[pfLoop]);
 		RecoObj pReco;
 		pReco.pt  = itPF->pt();
 		pReco.eta = itPF->eta();
@@ -114,11 +114,6 @@ const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
 		const pat::PackedCandidate *lPack = dynamic_cast<const pat::PackedCandidate* >(&(*itPF));
 		const edm::Ptr<pat::PackedCandidate > lPackEdmPtr = PFCol[pfLoop];
 
-		
-
-		//int legacyPVindex = 0; //use this dummy for now. Will eventually get filled with info from legacy vertex selector. 
-
-	//	reco::Vertex lpv = pvCol->at(legacyPVindex); //lpv = legacy primary vertex
 
 		Handle<flashgg::VertexCandidateMap> vtxmap; //access the flashgg  vtx->PackedCandidate Map 
 		iEvent.getByToken(vertexCandidateMapToken_,vtxmap);
@@ -129,28 +124,22 @@ const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
 			const edm::Ptr<reco::Vertex>  currentVertex = (vi->first); 
 
 			// the arugment of the if returns 1 if lPack is in the vector of PackedCandidates corresponding to currentVertex in the Map.
-	
-	//if (std::count((vtxmap->at(currentVertex).begin()),vtxmap->at(currentVertex).end(),*lPackEdmPtr)) 
 			if (std::count((vtxmap->at(currentVertex).begin()),vtxmap->at(currentVertex).end(),itPF)) 
 			{	
-				// by defintiion, this is therefore the closest vertex.
+				// by definition, this is therefore the closest vertex.
 				closestVtx = &(*currentVertex);
-		//std::cout << "DEBUG " << lpv->position()<< " "<< currentVertex->position() <<std::endl ;
 				//Now check if the currentVertex is the same as the legacy PV.
-				//Trying to do if (lpv == *currentVertex) gave weird compilation errors, so matchign positions is next best thing.
-				if (lpv->position() == currentVertex->position())
+				//Trying to do if (lpv == *currentVertex) gave weird compilation errors, so matching positions is next best thing.
+				if (std::fabs(lpv->position().Z() - currentVertex->position().Z())< 0.001 )
 				{
 					pVtxId =0; //confusingly, pVtxId == 0 when the PackedCandidate's associated vertex matches the Primary Vertex.
 					// This is a hangover from the code, which was originally designed such that the 0th vertex in the vertex collection was the PV
 					// In that case it would have made sense for pVtxId to be 0 when the vetrex was indeed the PV (ie 0th).
 					// Now we are left with slightly weird notation... to be cleared up someday, but works for now.
 					counterFindVtx++;
-					//		std::cout <<"[debug]  from LV" << std::endl; 
-				 break;
+					break;
 				}
 				pVtxId =1; // conversely, pVtxId >0 when the particle comes from a vertex other than the PV.
-
-				//	std::cout <<"[debug] NOT from LV" << std::endl; 
 			}
 		}
 		pDZ = lPack->dz(lpv->position()); //Unlike the original code, these quantities need to be calculated from the new PV, not 0th vtx.
@@ -158,9 +147,8 @@ const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
 		pD0 = lPack->dxy(lpv->position());
 		pReco.dZ = pDZ;
 		pReco.d0 = pD0;
-		//std::cout<<"DEBUG pvtxid "<<  pVtxId << ", closestVtx==0? "<<  (closestVtx==0) << std::endl;
-		//	std::cout<< "DEBUG " << lPack->vertex().Z()  << ", charge " << lPack->charge() << ", pdz " << pDZ << std::endl;
-			if(closestVtx == 0) pReco.vtxId = -1;// means no matchign vertex from map to lpv is found.
+		
+		if(closestVtx == 0) pReco.vtxId = -1;// means no matchign vertex from map to lpv is found.
 		if(closestVtx != 0) pReco.vtxId = pVtxId;
 		//if(closestVtx != 0) pReco.vtxChi2 = closestVtx->trackWeight(itPF->trackRef());
 		//Set the id for Puppi Algo: 0 is neutral pfCandidate, id = 1 for particles coming from PV and id = 2 for charged particles from non-leading vertex
@@ -173,43 +161,34 @@ const PtrVector<pat::PackedCandidate>& PFCol = pfCandidates->ptrVector();
 		//std::cout << "pVtxId = " << pVtxId << ", and charge = " << itPF->charge() << ", and closestVtx = " << closestVtx << ", and id = " << pReco.id << std::endl;
 		if (pReco.id==0) counter0++;
 		fRecoObjCollection.push_back(pReco);
-	}
-/*	std::cout << "DEBUG counter0 " << counter0<< std::endl;
-	std::cout << "DEBUG counter1 " << counter1<< std::endl;
-	std::cout << "DEBUG counter2 " << counter2<< std::endl;
-	std::cout << "DEBUG counter3 " << counter3<< std::endl;
-	std::cout << "DEBUG counter4 " << counter4<< std::endl;
-	std::cout << "DEBUG counterFindVtx " << counterFindVtx<< std::endl;*/
-	fPuppiContainer->initialize(fRecoObjCollection);
-	//Compute the weights and the candidates
-	const std::vector<double> lWeights = fPuppiContainer->puppiWeights();
-	//Fill it into the event
-	std::auto_ptr<edm::ValueMap<float> > lPupOut(new edm::ValueMap<float>());
-	edm::ValueMap<float>::Filler lPupFiller(*lPupOut);
-	//lPupFiller.insert(hPFProduct,lWeights.begin(),lWeights.end());
-	lPupFiller.insert(pfCandidates,lWeights.begin(),lWeights.end());
-	lPupFiller.fill();
-	iEvent.put(lPupOut,"PuppiWeights");
+		}
 
-	//Fill a new PF Candidate Collection
-	const std::vector<fastjet::PseudoJet> lCandidates = fPuppiContainer->puppiParticles();
+fPuppiContainer->initialize(fRecoObjCollection);
+//Compute the weights and the candidates
+const std::vector<double> lWeights = fPuppiContainer->puppiWeights();
+//Fill it into the event
+std::auto_ptr<edm::ValueMap<float> > lPupOut(new edm::ValueMap<float>());
+edm::ValueMap<float>::Filler lPupFiller(*lPupOut);
+//lPupFiller.insert(hPFProduct,lWeights.begin(),lWeights.end());
+lPupFiller.insert(pfCandidates,lWeights.begin(),lWeights.end());
+lPupFiller.fill();
+iEvent.put(lPupOut,"PuppiWeights");
 
-	fPuppiCandidates.reset( new vector<reco::PFCandidate> );    
-	for(unsigned int i0 = 0; i0 < lCandidates.size(); i0++) {
-		//reco::PFCandidate pCand;
-		//edm::FwdPtr<reco::PFCandidate> pCand(PFCol->at(lCandidates[i0].user_index()).charge(),
-		reco::PFCandidate pCand(PFCol[lCandidates[i0].user_index()]->charge(),
-				PFCol[lCandidates[i0].user_index()]->p4(),
-				translatePdgIdToType(PFCol[lCandidates[i0].user_index()]->pdgId()));
-		LorentzVector pVec; //pVec.SetPtEtaPhiM(lCandidates[i0].pt(),lCandidates[i0].eta(),lCandidates[i0].phi(),lCandidates[i0].Mass());
-		pVec.SetPxPyPzE(lCandidates[i0].px(),lCandidates[i0].py(),lCandidates[i0].pz(),lCandidates[i0].E());
-		pCand.setP4(pVec);
-		//pCand.get()->setP4(pVec);
-		fPuppiCandidates->push_back(pCand);
-		//fPuppiCandidates->push_back(pCand);
-	}
+//Fill a new PF Candidate Collection
+const std::vector<fastjet::PseudoJet> lCandidates = fPuppiContainer->puppiParticles();
 
-	iEvent.put(fPuppiCandidates,fPuppiName);
+fPuppiCandidates.reset( new vector<reco::PFCandidate> );    
+for(unsigned int i0 = 0; i0 < lCandidates.size(); i0++) {
+	reco::PFCandidate pCand(PFCol[lCandidates[i0].user_index()]->charge(),
+			PFCol[lCandidates[i0].user_index()]->p4(),
+			translatePdgIdToType(PFCol[lCandidates[i0].user_index()]->pdgId()));
+	LorentzVector pVec; //pVec.SetPtEtaPhiM(lCandidates[i0].pt(),lCandidates[i0].eta(),lCandidates[i0].phi(),lCandidates[i0].Mass());
+	pVec.SetPxPyPzE(lCandidates[i0].px(),lCandidates[i0].py(),lCandidates[i0].pz(),lCandidates[i0].E());
+	pCand.setP4(pVec);
+	fPuppiCandidates->push_back(pCand);
+}
+
+iEvent.put(fPuppiCandidates,fPuppiName);
 
 }
 // ------------------------------------------------------------------------------------------
